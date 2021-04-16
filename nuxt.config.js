0c2db25c6b44e48153f95ad0a4ea818f095efc75
Jordan McDougall <jordanmcdougall@hotmail.co.uk>
2021-04-16 21:47:32 +0100

    fix: add  static route generation

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,6 +42,14 @@ export default {
     '@nuxt/content',
   ],
 
+  generate: {
+    async routes() {
+      const { $content } = require('@nuxt/content')
+      const files = await $content({ deep: true }).only(['path']).fetch()
+
+      return files.map((file) => (file.path === '/index' ? '/' : file.path))
+    },
+  },
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
