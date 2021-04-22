e6ee3ce9b4e171a54cadd508420e488ef39a6a0f
Jordan McDougall <jordanmcdougall@hotmail.co.uk>
2021-04-22 12:53:40 +0100

    fix: google analytics

--- layouts/default.vue
+++ layouts/default.vue
@@ -28,6 +28,18 @@ export default {
       return new Date().getFullYear()
     },
   },
+  mounted() {
+    window.dataLayer = window.dataLayer || []
+    function gtag() {
+      // eslint-disable-next-line no-undef
+      dataLayer.push(arguments)
+    }
+    gtag('js', new Date())
+
+    gtag('config', 'G-DKQYV6KT7N')
+
+    console.log(gtag())
+  },
 }
 </script>
 
