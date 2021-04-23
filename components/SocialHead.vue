35f3218acdc834b3bad1834bbada539955dae109
Jordan McDougall <jordanmcdougall@hotmail.co.uk>
2021-04-23 13:31:11 +0100

    fix: update url

--- components/SocialHead.vue
+++ components/SocialHead.vue
@@ -47,7 +47,7 @@ export default {
         {
           hid: 'twitter:image',
           name: 'twitter:image',
-          content: `https://vigilant-agnesi-0201e1.netlify.app${this.image}`,
+          content: `https://jordanmcdougall.dev${this.image}`,
         },
         {
           hid: 'twitter:site',
--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -74,7 +74,7 @@ export default {
       if (navigator.share) {
         navigator.share({
           title: this.page.title,
-          url: 'https://vigilant-agnesi-0201e1.netlify.app' + this.$route.path,
+          url: 'https://jordanmcdougall.dev' + this.$route.path,
         })
         // .catch(console.error('oops'))
       } else {
