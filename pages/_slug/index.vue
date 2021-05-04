1e21d39f68d8ea74af502bb00c7cb2d91a716c94
Jordan McDougall <jordanmcdougall@hotmail.co.uk>
2021-05-04 10:56:14 +0100

    feat: add figure caption

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -24,7 +24,7 @@
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
-          class="h-5 w-5"
+          class="h-5 w-5 cursor-pointer"
           @click="sharePost()"
         >
           <path
@@ -36,7 +36,22 @@
         </svg>
       </div>
     </span>
-    <img :src="page.image" alt="page.title" class="px-10 pb-10 mx-auto" />
+    <figure>
+      <img :src="page.image" alt="page.title" class="px-10 mx-auto" />
+      <figcaption class="px-10 pb-10 text-gray-600 italic">
+        {{ page.image_desc }}
+      </figcaption>
+    </figure>
+    <h2 class="px-10 font-serif">Table of Contents</h2>
+    <ul>
+      <li
+        v-for="link in page.toc"
+        :key="link.id"
+        :class="{ toc2: link.depth === 2, toc3: link.depth === 3 }"
+      >
+        <NuxtLink :to="`#${link.id}`">{{ link.text }}</NuxtLink>
+      </li>
+    </ul>
 
     <nuxt-content
       class="font-serif text-xl text-justify px-5 grid-rows-1"
@@ -70,7 +85,6 @@ export default {
   },
   methods: {
     sharePost() {
-      console.log(this.$route)
       if (navigator.share) {
         navigator.share({
           title: this.page.title,
@@ -96,16 +110,43 @@ h2:not(.subtitle) {
   padding: auto;
   @apply text-3xl;
   @apply font-bold;
+  @apply mb-5;
 }
 
 h3 {
   padding: auto;
   @apply text-xl;
   @apply font-bold;
+  @apply mb-5;
 }
 
 p {
-  @apply mb-10;
+  @apply mb-5;
+}
+
+ul {
+  @apply mb-5;
+  @apply list-disc;
+  @apply px-16;
+}
+
+ol {
+  @apply mb-5;
+  @apply list-decimal;
+  @apply px-6;
+}
+
+.toc2 {
+  @apply text-xl;
+}
+
+a:hover {
+  @apply underline;
+  @apply cursor-pointer;
+}
+
+figcaption {
+  margin-top: 0;
 }
 
 h2:not(.subtitle):hover > a > .icon.icon-link {
